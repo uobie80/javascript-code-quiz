2227de6b58132f54f0d860f3c1691c02bef39a43
Uchenna Obicheta <uobicheta@hotmail.com>
2022-12-09 23:51:10 -0500

    Implemented questions and choices to dynamically appear on page.

--- script.js
+++ script.js
@@ -1,32 +1,27 @@
 /* Select specific html tags to insert dynamic content into */
-var questionsEl1 = document.querySelector("#questions");
-var choiceEl1 = document.querySelector("#choices");
-var messageEl1 = document.querySelector("#message");
-var headerEl1 = document.querySelector("#header");
+var contentEl1 = document.querySelector("#header");
 var timeEl1 = document.querySelector("#time");
 
 /* Initialize time limit */
 time_limit = 100;
 
-/* Initialize array index for coding quesitons*/
-idx = 0;
 
 /* Create h1 tag and associated text */
 h1_text = document.createElement("h1");
 h1_text.textContent = "JavaScript Coding Challenge";
-headerEl1.appendChild(h1_text);
+contentEl1 .appendChild(h1_text);
 
 /* Create p tag and add text to it */
 p1_text = document.createElement("p");
 p1_text.textContent = "Try to answer the following code related questions within the time limit.\
                        Keep in mind that incorrect answers will be penalize your score/time by ten seconds!";
-headerEl1.appendChild(p1_text);
+contentEl1 .appendChild(p1_text);
 
 /* Create button to start coding quiz */
 start_button = document.createElement("button");
 start_button.textContent = "Start Quiz";
 start_button.setAttribute("id", "start-quiz");
-headerEl1.appendChild(start_button);
+contentEl1 .appendChild(start_button);
 
 
 /* Set the quiz timer */
@@ -45,12 +40,12 @@ questions = ["Statements should end with a ____.",
              "Which property is used to determine the size of an array?"];
 
 /* Define list of answers to coding questions */             
-correct_answer = [";", "0", "datatype", "iterate", "increment",
+correct_answers = [";", "0", "datatype", "iterate", "increment",
                   "decrement", "less than", "greater than", "declare a variable",
                   "length"];
 
 /* Define list of wrong choices to coding questions */
-wrong_choices = [
+     choices = [
                  ["}", ";", "[", "?"],
                  ["1", "0", "-1", "@"],
                  ["datatype", "variable", "descriptor", "entity"],
@@ -65,20 +60,44 @@ wrong_choices = [
 
 function start_quiz() {
    
-    /* This function loops through each question and choices and displays it on 
-    on the page */
+    /* 
+       This function loops through each question 
+       and choices and displays it on the page. 
+    */
 
-    /* Create html elements */
-    OrderedListEl1 = document.createElement("ul");
-    listEl1 = document.createElement("li");
-    listEl2 = document.createElement("li");
-    listEl3 = document.createElement("li");
-    listEl4 = document.createElement("li");
-    
-    for (var i=0; i < questions.lenth; i++) {
-          
-    }
-    
+/* Initialize array index for coding quesitons*/
+idx = 0;
+
+/* Select html element */
+var contentEl1 = document.querySelector("#header");
+var question = questions[idx];
+contentEl1.textContent = question;
+
+/* Create html elements */
+OrderedListEl1 = document.createElement("ol");
+listEl1 = document.createElement("li");
+listEl2 = document.createElement("li");
+listEl3 = document.createElement("li");
+listEl4 = document.createElement("li");
+
+/* Get choices */
+var choice = choices[idx];
+
+/* Set choices as content for list items */
+listEl1.textContent = choice[3];
+listEl2.textContent = choice[2];
+listEl3.textContent = choice[0];
+listEl4.textContent = choice[1];
+
+/* Add list items to ordered list */
+OrderedListEl1.appendChild(listEl1);
+OrderedListEl1.appendChild(listEl2);
+OrderedListEl1.appendChild(listEl3);
+OrderedListEl1.appendChild(listEl4);
+
+contentEl1.appendChild(OrderedListEl1);
+
 }
+
 
 start_button.addEventListener("click", start_quiz);
